578f16d98a93ee3a5802cacf501c5e22d067fd14
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-09-08 15:54:27 -0400

    added system colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
+    colors: {
+      "azul": "#6686B7",
+      "bondi": "#308F91",
+      "copper": "#CC6633",
+      "crimson": "#CC3333",
+      "emerald": "#339966",
+      "french-blue": "#666699",
+      "gold": "#999900",
+      "ivy": "#336633",
+      "lavender": "#6666CC",
+      "magenta": "#993399",
+      "nutmeg": "#996633",
+      "pistachio": "#B5E040",
+      "plum": "#996699",
+      "poppy": "#F46F19",
+      "rose": "#996666",
+      "sapphire": "#3366FF",
+      "silver": "#777777",
+      "sunny": "#D0AF3E",
+      "teal": "#477979",
+      "turquoise": "#0099CC"
+    }
   },
   plugins: [],
 }
