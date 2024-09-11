bfdb2e03f39f3e3018ee51a6dc76a0969f784053
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-09-11 00:42:28 -0400

    draggable and fucksing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script>
 	import Window from '$lib/components/window/Window.svelte';
+	import StickyNote from '$lib/components/sticky_note/StickyNote.svelte';
 </script>
 
-<Window name="FAQs">
+<!-- <Window name="FAQs">
 	<slot slot="icon">
 		<img class="disable-blur w-[13px]" src="/assets/folders/help.svg" alt="Help" />
 	</slot>
 
 	<div class="px-1" style="width: 350px; height: 200px;">testing</div>
-</Window>
+</Window> -->
+
+<div class="h-screen w-screen bg-bondi">
+	<StickyNote initialMessage="This is a bum ass message bro" />
+	<StickyNote initialMessage="Another note" />
+</div>
