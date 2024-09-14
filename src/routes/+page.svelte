5f113e50866adb8471f0edbef5f0ba78a2288476
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-09-13 21:39:58 -0400

    got store working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Window from '$lib/components/window/Window.svelte';
 	import StickyNote from '$lib/components/sticky_note/StickyNote.svelte';
+	import { openApps, AppType, addSticky } from '$lib/stores/apps.store';
 </script>
 
 <!-- <Window name="FAQs">
@@ -11,7 +12,12 @@
 	<div class="px-1" style="width: 350px; height: 200px;">testing</div>
 </Window> -->
 
-<div class="h-screen w-screen bg-bondi">
-	<StickyNote initialMessage="This is a bum ass message bro" />
-	<StickyNote initialMessage="Another note" />
+<div class="h-screen w-screen bg-blueberry-oxygen bg-cover bg-no-repeat bg-center">
+
+	<button on:click={addSticky}>Add Sticky Note</button>
+	{#each Object.entries($openApps) as [id, appType]}
+		{#if appType === AppType.STICKY}
+			<StickyNote initialMessage={`${id}`} />
+		{/if}
+	{/each}
 </div>
