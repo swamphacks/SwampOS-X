df5ea19a4c624da8f6e19adfd6b40d44ca261f8e
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-09-13 22:55:43 -0400

    the shit was some bum but it works now

--- src/lib/stores/apps.store.ts
+++ src/lib/stores/apps.store.ts
@@ -1,22 +1,46 @@
-import { writable } from "svelte/store";
+import { get, writable } from 'svelte/store';
 
 export enum AppType {
-  WINDOW,
-  STICKY
+	WINDOW,
+	STICKY
 }
 
 let stickyCount = 0;
 
 export const openApps = writable<Record<string, AppType>>({});
+export const zindexApp = writable<Record<string, number>>({});
+
+export const activeApp = writable<string | null>(null);
+
+export const activeAppZIndex = writable(0);
 
 export const addSticky = () => {
-  const id = `sticky-${stickyCount}`
-  stickyCount += 1;
+	const id = `sticky-${stickyCount}`;
+	stickyCount += 1;
 
-  openApps.update((store) => {
-    return {
-      ...store,
-      [id]: AppType.STICKY
-    }
-  })
-}
+	openApps.update((store) => ({
+		...store,
+		[id]: AppType.STICKY
+	}));
+
+	zindexApp.update((store) => ({
+		...store,
+		[id]: 0
+	}));
+
+	setActiveApp(id);
+};
+
+export const setActiveApp = (id: string) => {
+	if (id in get(zindexApp)) {
+		const curr_index = get(activeAppZIndex);
+		const new_index = curr_index + 1;
+		zindexApp.update((store) => ({
+			...store,
+			[id]: new_index
+		}));
+
+		activeAppZIndex.set(new_index);
+		activeApp.set(id);
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Window from '$lib/components/window/Window.svelte';
 	import StickyNote from '$lib/components/sticky_note/StickyNote.svelte';
 	import { openApps, AppType, addSticky } from '$lib/stores/apps.store';
 </script>
@@ -12,12 +11,11 @@
 	<div class="px-1" style="width: 350px; height: 200px;">testing</div>
 </Window> -->
 
-<div class="h-screen w-screen bg-blueberry-oxygen bg-cover bg-no-repeat bg-center">
-
+<div class="h-screen w-screen bg-blueberry-oxygen bg-cover bg-center bg-no-repeat">
 	<button on:click={addSticky}>Add Sticky Note</button>
 	{#each Object.entries($openApps) as [id, appType]}
 		{#if appType === AppType.STICKY}
-			<StickyNote initialMessage={`${id}`} />
+			<StickyNote initialMessage={`${id}`} appID={id} />
 		{/if}
 	{/each}
 </div>
