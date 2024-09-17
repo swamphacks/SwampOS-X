3535d108fc71c848b612e71b8c4e26838d74808a
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-17 19:27:16 -0400

    make icons disappear when control is dragged

--- src/lib/components/control_strip/ControlStrip.svelte
+++ src/lib/components/control_strip/ControlStrip.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { draggable, type DragEventData } from '@neodrag/svelte';
+
 	let apps = [
 		{ src: 'apple_talk.svg', clicked: false, display: true },
 		{ src: 'cd.svg', clicked: false, display: true },
@@ -24,18 +26,47 @@
 		appsClosed = !appsClosed;
 		apps = apps.map((app) => ({ ...app, display: !appsClosed }));
 	};
+
+	const onControlDrag = (e: CustomEvent<DragEventData>) => {
+		if (appsClosed || !rightArrowElement) return;
+		const offset = 32;
+		const rightArrowPos = parseInt(controlElement.style.left.slice(0, -2)) - 17 + e.detail.offsetX;
+		rightArrowElement.style.left = rightArrowPos + 'px';
+
+		apps = apps.map((app, idx) => {
+			if (idx * 24 - idx < rightArrowPos - offset) {
+				return { ...app, display: true };
+			}
+			return { ...app, display: false };
+		});
+
+		if (rightArrowPos < 17) {
+			leftArrowElement.style.visibility = 'hidden';
+		} else {
+			leftArrowElement.style.visibility = 'visible';
+		}
+
+		if (rightArrowPos < 0) {
+			closeElement.style.visibility = 'hidden';
+		} else {
+			closeElement.style.visibility = 'visible';
+		}
+	};
 </script>
 
-<section class="relative mt-5 flex flex-row items-center">
+<section
+	class="relative mt-5 flex h-fit"
+	style="width: {appsClosed ? 17 : 16 + 17 + 24 * apps.length + 17 + 17 - apps.length - 3}px;"
+>
 	{#if !appsClosed}
 		<button
-			class="h-[26px] w-[16px] bg-[url(assets/control_strip/controls/inactive/close.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/close.svg)]"
+			class="absolute left-0 h-[26px] w-[16px] bg-[url(assets/control_strip/controls/inactive/close.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/close.svg)]"
 			bind:this={closeElement}
 			on:click={toggleApps}
 		/>
 
 		<button
-			class="relative left-[-1px] h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/left-arrow.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/left-arrow.svg)]"
+			class="absolute left-[15px] h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/left-arrow.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/left-arrow.svg)]"
 			bind:this={leftArrowElement}
 		/>
 	{/if}
@@ -43,8 +74,8 @@
 	{#each apps as app, idx}
 		{#if app.display}
 			<button
-				class="relative h-[26px] w-[24px] select-none bg-[url(assets/control_strip/controls/inactive/icon-container.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/icon-container.svg)]"
-				style="left: -{idx + 2}px;"
+				class="absolute h-[26px] w-[24px] select-none bg-[url(assets/control_strip/controls/inactive/icon-container.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/icon-container.svg)]"
+				style="left: {30 + idx * 24 - idx}px;"
 			>
 				<img
 					src={`assets/control_strip/app_icons/${app.src}`}
@@ -59,16 +90,17 @@
 
 	{#if !appsClosed}
 		<button
-			class="relative h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/right-arrow.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/right-arrow.svg)]"
-			style="{!appsClosed && 'left: -' + (apps.length + 2) + 'px'}; background-image: "
+			class="absolute h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/right-arrow.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/right-arrow.svg)]"
+			style={'left: ' + (30 + apps.length * 24 - apps.length) + 'px;'}
 			bind:this={rightArrowElement}
 		/>
 	{/if}
 
 	<button
-		class="relative h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/control.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/control.svg)]"
-		style="{appsClosed ? 'left: -1px' : 'left: -' + (apps.length + 2) + 'px'};"
+		class="absolute h-[26px] w-[17px] bg-[url(assets/control_strip/controls/inactive/control.svg)] bg-center bg-no-repeat active:bg-[url(assets/control_strip/controls/active/control.svg)]"
+		style={appsClosed ? 'left: 0' : 'left: ' + (30 + apps.length * 24 - apps.length + 17) + 'px;'}
+		use:draggable={{ axis: 'x', bounds: 'parent' }}
+		on:neodrag={onControlDrag}
 		bind:this={controlElement}
-		on:click={toggleApps}
 	/>
 </section>
