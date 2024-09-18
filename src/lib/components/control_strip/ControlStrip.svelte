3ae62522414cfa8074f5a36cec897cf92c97e9ed
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-18 18:19:34 -0400

    change icons to png and add keyboard support

--- src/lib/components/control_strip/ControlStrip.svelte
+++ src/lib/components/control_strip/ControlStrip.svelte
@@ -5,17 +5,67 @@
 	const iconWidth = 24;
 
 	let apps = [
-		{ src: 'apple_talk.svg', clicked: false, display: true },
-		{ src: 'cd.svg', clicked: false, display: true },
-		{ src: 'file_sharing.svg', clicked: false, display: true },
-		{ src: 'itunes.svg', clicked: false, display: true },
-		{ src: 'keychain_strip.svg', clicked: false, display: true },
-		{ src: 'monitor_resolution.svg', clicked: false, display: true },
-		{ src: 'remote_access.svg', clicked: false, display: true },
-		{ src: 'monitor_bitdepth.svg', clicked: false, display: true },
-		{ src: 'printer.svg', clicked: false, display: true },
-		{ src: 'sound_volume.svg', clicked: false, display: true },
-		{ src: 'web_sharing.svg', clicked: false, display: true }
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'apple_talk.png',
+			clicked: false,
+			display: true
+		},
+		{ el: undefined as unknown as HTMLButtonElement, src: 'cd.png', clicked: false, display: true },
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'file_sharing.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'itunes.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'keychain_strip.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'monitor_resolution.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'remote_access.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'monitor_bitdepth.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'printer.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'sound_volume.png',
+			clicked: false,
+			display: true
+		},
+		{
+			el: undefined as unknown as HTMLButtonElement,
+			src: 'web_sharing.png',
+			clicked: false,
+			display: true
+		}
 	];
 
 	let controlElement: HTMLButtonElement;
@@ -43,7 +93,22 @@
 			rightArrowElement.style.left = -17 + 'px';
 			position.x = 0;
 		}
+		activateArrows(!appsVisible);
 		positionPrev = position.x;
+	};
+
+	const activateArrows = (active: boolean) => {
+		if (active) {
+			leftArrowElement.classList.remove('inactive-left');
+			rightArrowElement.classList.remove('inactive-right');
+			leftArrowElement.classList.add('active-left');
+			rightArrowElement.classList.add('active-right');
+		} else {
+			leftArrowElement.classList.remove('active-left');
+			rightArrowElement.classList.remove('active-right');
+			leftArrowElement.classList.add('inactive-left');
+			rightArrowElement.classList.add('inactive-right');
+		}
 	};
 
 	const onControlDrag = (e: CustomEvent<DragEventData>) => {
@@ -110,6 +175,8 @@
 			appsVisible = true;
 		}
 
+		console.log(apps.some((app) => !app.display));
+		activateArrows(apps.some((app) => !app.display));
 		positionPrev = position.x;
 	};
 
@@ -135,22 +202,29 @@
 	style="width: {iconOffset + iconWidth * apps.length + 17 + 17 - apps.length}px;"
 >
 	<button
-		class="absolute left-0 h-[26px] w-[16px] bg-[url(/assets/control_strip/controls/inactive/close.svg)] bg-center bg-no-repeat active:bg-[url(/assets/control_strip/controls/active/close.svg)]"
+		class="absolute left-0 h-[26px] w-[16px] bg-[url(/assets/control_strip/controls/default/close.png)] bg-center bg-no-repeat focus:bg-[url(/assets/control_strip/controls/active/close.png)] active:bg-[url(/assets/control_strip/controls/active/close.png)]"
+		tabindex="0"
 		bind:this={closeElement}
 		on:click={toggleApps}
 	/>
 	<button
-		class="absolute left-[15px] h-[26px] w-[17px] bg-[url(/assets/control_strip/controls/inactive/left-arrow.svg)] bg-center bg-no-repeat"
-		class:active-left={apps.some((app) => !app.display)}
+		class="inactive-left absolute left-[15px] h-[26px] w-[17px] bg-center bg-no-repeat focus:bg-[url(/assets/control_strip/controls/active/left_arrow.png)]"
+		tabindex="0"
 		bind:this={leftArrowElement}
 		on:click={shiftAppsLeft}
+		on:mouseup={() => leftArrowElement.blur()}
+		on:mouseleave={() => leftArrowElement.blur()}
 	/>
 
 	{#each apps as app, idx}
 		{#if app.display}
 			<button
-				class="absolute h-[26px] w-[24px] select-none bg-[url(/assets/control_strip/controls/inactive/icon-container.svg)] bg-center bg-no-repeat active:bg-[url(/assets/control_strip/controls/active/icon-container.svg)]"
+				class="absolute h-[26px] w-[24px] select-none bg-[url(/assets/control_strip/controls/default/app.png)] bg-center bg-no-repeat focus:bg-[url(/assets/control_strip/controls/active/app.png)] active:bg-[url(/assets/control_strip/controls/active/app.png)]"
 				style="left: {iconOffset + idx * iconWidth - idx}px;"
+				bind:this={app.el}
+				tabindex="0"
+				on:mouseup={() => app.el.blur()}
+				on:mouseleave={() => app.el.blur()}
 			>
 				<img
 					src={`/assets/control_strip/app_icons/${app.src}`}
@@ -164,23 +238,28 @@
 	{/each}
 
 	<button
-		class="absolute h-[26px] w-[17px] bg-[url(/assets/control_strip/controls/inactive/right-arrow.svg)] bg-center bg-no-repeat"
+		class="inactive-right absolute h-[26px] w-[17px] bg-center bg-no-repeat focus:bg-[url(/assets/control_strip/controls/active/right_arrow.png)]"
 		style={'left: ' + (iconOffset + apps.length * iconWidth - apps.length) + 'px;'}
-		class:active-right={apps.some((app) => !app.display)}
+		tabindex="0"
 		bind:this={rightArrowElement}
+		on:mouseup={() => rightArrowElement.blur()}
+		on:mouseleave={() => rightArrowElement.blur()}
 		on:click={shiftAppsRight}
 	/>
 
 	<button
-		class="absolute h-[26px] w-[17px] bg-[url(/assets/control_strip/controls/inactive/control.svg)] bg-center bg-no-repeat active:bg-[url(/assets/control_strip/controls/active/control.svg)]"
+		class="absolute h-[26px] w-[17px] bg-[url(/assets/control_strip/controls/default/controller.png)] bg-center bg-no-repeat focus:bg-[url(/assets/control_strip/controls/active/controller.png)] active:bg-[url(/assets/control_strip/controls/active/controller.png)]"
 		use:draggable={{
 			position,
 			axis: 'x',
 			bounds: 'parent'
 		}}
+		tabindex="0"
 		on:neodrag={onControlDrag}
 		on:neodrag:end={onControlDragEnd}
 		bind:this={controlElement}
+		on:mouseup={() => controlElement.blur()}
+		on:mouseleave={() => controlElement.blur()}
 		on:click={() => {
 			if (!controlDragged) toggleApps();
 			controlDragged = false;
@@ -189,11 +268,23 @@
 </section>
 
 <style lang="postcss">
-	.active-right {
-		@apply active:bg-[url(/assets/control_strip/controls/active/right-arrow.svg)];
+	:global(.active-right) {
+		@apply bg-[url(/assets/control_strip/controls/default/right_arrow.png)] active:bg-[url(/assets/control_strip/controls/active/right_arrow.png)];
 	}
 
-	.active-left {
-		@apply active:bg-[url(/assets/control_strip/controls/active/left-arrow.svg)];
+	:global(.active-left) {
+		@apply bg-[url(/assets/control_strip/controls/default/left_arrow.png)] active:bg-[url(/assets/control_strip/controls/active/left_arrow.png)];
+	}
+
+	:global(.inactive-left) {
+		@apply cursor-default bg-[url(/assets/control_strip/controls/disabled/left_arrow.png)];
+	}
+
+	:global(.inactive-right) {
+		@apply cursor-default bg-[url(/assets/control_strip/controls/disabled/right_arrow.png)];
+	}
+
+	button {
+		@apply focus:outline-none focus:ring-0 focus:ring-offset-0;
 	}
 </style>
