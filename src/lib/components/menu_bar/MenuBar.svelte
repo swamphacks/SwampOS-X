f4726c25ff2e8e6029d166e2f59a69b340a8637e
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-29 23:33:40 -0400

    add finder

--- src/lib/components/menu_bar/MenuBar.svelte
+++ src/lib/components/menu_bar/MenuBar.svelte
@@ -1,49 +1,63 @@
 <section
-	class="flex h-[30px] w-screen items-center border-y-2 border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] px-[8px]"
+	class="flex h-[30px] w-screen items-center border-y-2 border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] px-[8px] font-chicago text-[13px] leading-[2]"
 >
 	<div class="flex items-center">
 		<img
-			class="h-[28px] px-2"
 			draggable="false"
 			src="/assets/menu_bar/swamphacks.png"
 			alt="SwampHacks"
+			height="28"
+			width="40"
 		/>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 01</button>
+			<button class="h-full group-hover:text-white">Menu 01</button>
 		</div>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 02</button>
+			<button class="h-full group-hover:text-white">Menu 02</button>
 		</div>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 03</button>
+			<button class="h-full group-hover:text-white">Menu 03</button>
 		</div>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 04</button>
+			<button class="h-full group-hover:text-white">Menu 04</button>
 		</div>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 05</button>
+			<button class="h-full group-hover:text-white">Menu 05</button>
 		</div>
 
 		<div class="btn-container group">
-			<button class="menu-btn group-hover:text-white">Menu 06</button>
+			<button class="h-full group-hover:text-white">Menu 06</button>
 		</div>
 	</div>
 
-	<div></div>
+	<div class="flex grow flex-row-reverse items-center">
+		<h1>Finder</h1>
+		<img
+			height="22"
+			width="38"
+			class="px-2"
+			draggable="false"
+			src="/assets/menu_bar/finder.png"
+			alt="Finder"
+		/>
+		<img
+			class="mt-[-2px] h-[28px]"
+			draggable="false"
+			src="/assets/menu_bar/resizer.png"
+			alt="Menu Bar Resizer"
+		/>
+		<button class="px-2">12:45 PM</button>
+	</div>
 </section>
 <span class="block h-[2px] w-screen bg-[#262626]" />
 
 <style lang="postcss">
 	.btn-container {
 		@apply flex h-[34px] items-center px-2 hover:bg-[#333399];
-	}
-
-	.menu-btn {
-		@apply h-full font-chicago text-[13px] leading-[2];
 	}
 </style>
