b8525e48dfb2fe738352e10388448ef8a103fbeb
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-30 08:47:27 -0400

    add support for mobile viewport

--- src/lib/components/menu_bar/MenuBar.svelte
+++ src/lib/components/menu_bar/MenuBar.svelte
@@ -4,7 +4,7 @@
 	function currentTime() {
 		const now = new Date();
 		const hours = now.getHours();
-		const minutes = now.getMinutes();
+		const minutes = now.getMinutes().toString().padStart(2, '0');
 		const half = hours < 12 ? 'AM' : 'PM';
 
 		let hourString;
@@ -25,17 +25,16 @@
 </script>
 
 <section
-	class="flex h-[30px] w-screen items-center border-y-2 border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] pr-[8px] font-chicago text-[13px] leading-[2]"
+	class="flex h-[13px] w-screen items-center border-y border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] pr-[8px] font-chicago text-[4px] min-[410px]:h-[15px] min-[410px]:text-[6px] min-[510px]:h-[20px] min-[510px]:border-y-2 min-[510px]:text-[8px] min-[660px]:h-[30px] min-[660px]:text-[12px]"
 >
 	<div class="flex items-center">
 		<div class="btn-container hover:bg-[#333399]">
 			<button class="h-full">
 				<img
 					draggable="false"
+					class="h-[15px] min-[510px]:h-[20px] min-[510px]:w-[30px] min-[660px]:h-[28px] min-[660px]:w-[42px]"
 					src="/assets/menu_bar/swamphacks.png"
 					alt="SwampHacks"
-					height="28"
-					width="40"
 				/>
 			</button>
 		</div>
@@ -68,23 +67,22 @@
 	<div class="flex grow flex-row-reverse items-center">
 		<h1>Finder</h1>
 		<img
+			width="22"
 			height="22"
-			width="38"
-			class="px-2"
-			draggable="false"
 			src="/assets/menu_bar/finder.png"
 			alt="Finder"
+			class="mr-[3px] h-[8px] w-[8px] min-[410px]:h-[12px] min-[410px]:w-[12px] min-[510px]:mr-1.5 min-[510px]:h-[16px] min-[510px]:w-[16px] min-[660px]:h-[22px] min-[660px]:w-[22px]"
 		/>
 		<img
-			class="mt-[-2px] h-[28px]"
+			class="mx-1 mt-[-2px] h-[13px] min-[410px]:mx-1 min-[410px]:h-[14px] min-[510px]:mx-1.5 min-[510px]:h-[18px] min-[660px]:h-[28px]"
 			draggable="false"
 			src="/assets/menu_bar/resizer.png"
 			alt="Menu Bar Resizer"
 		/>
-		<button class="px-2">{currTime}</button>
+		<button>{currTime}</button>
 	</div>
 </section>
-<span class="block h-[2px] w-screen bg-[#262626]" />
+<span class="block h-[1px] w-screen bg-[#262626] min-[510px]:h-[2px]" />
 
 <style lang="postcss">
 	.btn-container {
