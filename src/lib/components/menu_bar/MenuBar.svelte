86e199e51511cef2974b1bf31a0f87d842ef96d7
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-09-29 23:57:34 -0400

    setup real time and make logo button

--- src/lib/components/menu_bar/MenuBar.svelte
+++ src/lib/components/menu_bar/MenuBar.svelte
@@ -1,14 +1,44 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	function currentTime() {
+		const now = new Date();
+		const hours = now.getHours();
+		const minutes = now.getMinutes();
+		const half = hours < 12 ? 'AM' : 'PM';
+
+		let hourString;
+		if (hours === 0) hourString = '12';
+		else if (hours <= 12) hourString = hours.toString();
+		else hourString = (hours - 12).toString();
+
+		return `${hourString}:${minutes} ${half}`;
+	}
+
+	let currTime: string = currentTime();
+	let intervalId: number;
+
+	onMount(() => {
+		intervalId = setInterval(() => (currTime = currentTime()), 1000); // Update every second
+		return () => clearInterval(intervalId);
+	});
+</script>
+
 <section
-	class="flex h-[30px] w-screen items-center border-y-2 border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] px-[8px] font-chicago text-[13px] leading-[2]"
+	class="flex h-[30px] w-screen items-center border-y-2 border-solid border-b-[#BBBBBB] border-t-[#EEEEEE] bg-[#DDDDDD] pr-[8px] font-chicago text-[13px] leading-[2]"
 >
 	<div class="flex items-center">
-		<img
-			draggable="false"
-			src="/assets/menu_bar/swamphacks.png"
-			alt="SwampHacks"
-			height="28"
-			width="40"
-		/>
+		<div class="btn-container hover:bg-[#333399]">
+			<button class="h-full">
+				<img
+					draggable="false"
+					src="/assets/menu_bar/swamphacks.png"
+					alt="SwampHacks"
+					height="28"
+					width="40"
+				/>
+			</button>
+		</div>
 
 		<div class="btn-container group">
 			<button class="h-full group-hover:text-white">Menu 01</button>
@@ -51,13 +81,13 @@
 			src="/assets/menu_bar/resizer.png"
 			alt="Menu Bar Resizer"
 		/>
-		<button class="px-2">12:45 PM</button>
+		<button class="px-2">{currTime}</button>
 	</div>
 </section>
 <span class="block h-[2px] w-screen bg-[#262626]" />
 
 <style lang="postcss">
 	.btn-container {
-		@apply flex h-[34px] items-center px-2 hover:bg-[#333399];
+		@apply flex h-[34px] items-center px-1.5 hover:bg-[#333399];
 	}
 </style>
