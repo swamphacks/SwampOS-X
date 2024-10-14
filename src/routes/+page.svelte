0b424e4464aabf7eab65d37b7fc21b2150903dcd
Robert Conde <rconde2003@gmail.com>
2024-10-14 00:23:42 -0400

    on my mind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,32 @@
 	import Window from '$lib/components/window/Window.svelte';
 	import type { Position } from '$lib/utils/windows';
 
-	const randPos = (): Position => ({
-		x: (675 - 100) * Math.random(),
-		y: (750 - 100) * Math.random()
+	const heart = ({ x, y }: Position): boolean => {
+		// convert to [-1, 1]
+		x = x * 2 - 1;
+		y = y * 2 - 1;
+
+		const C = 1 - Math.cbrt(x * x);
+		return C * Math.pow(19 - 8 * C, 2) - Math.pow(6 - 10 * C - 16 * y, 2) >= 1;
+	};
+
+	const randPos = (): Position => {
+		let pos: Position;
+		do {
+			pos = {
+				x: Math.random(),
+				y: Math.random()
+			};
+		} while (!heart(pos));
+		return pos;
+	};
+
+	const toCanvas = (pos: Position): Position => ({
+		x: 785 * pos.x,
+		y: 785 * (1 - pos.y)
 	});
 
-	const K = 50;
+	const K = 100;
 </script>
 
 <MenuBar />
@@ -61,11 +81,6 @@
 
 {#each Array.from({ length: K }) as _, i}
 	{#each Object.keys(stickyNoteColors) as color}
-		<StickyNote
-			text={`this is where you belong ;)\n\n(${i + 1}/${K} in ${color})`}
-			{color}
-			startAt={randPos()}
-			size={{ w: 100, h: 100 }}
-		/>
+		<StickyNote text={';)'} {color} startAt={toCanvas(randPos())} size={{ w: 20, h: 15 }} />
 	{/each}
 {/each}
