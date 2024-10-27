fcb09c88eee9eb7c167685a8c23f971db8c6c824
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-10-27 12:18:04 -0400

    make arrow reactive to open/close state

--- src/lib/components/faq/Accordion.svelte
+++ src/lib/components/faq/Accordion.svelte
@@ -2,13 +2,36 @@
 	export let title: string;
 	let arrow: HTMLElement;
 
-	// let opened = false;
+	let opened = false;
+
+	function onClick() {
+		opened = !opened;
+	}
 </script>
 
 <div>
-	<button class="flex items-center gap-4">
-		<img src="/assets/faq/arrow.png" alt="arrow" bind:this={arrow} height={8} width={15} />
+	<button class="flex items-center gap-4" on:click={onClick}>
+		<img
+			src="/assets/faq/arrow.png"
+			alt="arrow"
+			class={opened ? 'open-arrow' : 'close-arrow'}
+			bind:this={arrow}
+			height={8}
+			width={15}
+		/>
 		<img src="/assets/faq/help.png" alt={title} width={33} height={33} />
 		<h1 class="text-lg">{title}</h1>
 	</button>
 </div>
+
+<style lang="postcss">
+	.open-arrow {
+		transform: rotate(360deg);
+		transition: transform 0.3s ease;
+	}
+
+	.close-arrow {
+		transform: rotate(270deg);
+		transition: transform 0.3s ease;
+	}
+</style>
