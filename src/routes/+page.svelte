030e574b6031c579b74862528112bf45404254aa
Alexander Wang <alexander.yisu.wang@outlook.com>
2024-10-28 19:44:56 -0400

    getting familiar with the current window

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,36 @@
 <script lang="ts">
 	import MenuBar from '$lib/components/menu_bar/MenuBar.svelte';
 	import ControlStrip from '$lib/components/control_strip/ControlStrip.svelte';
-	import { stickyNoteColors } from '$lib/components/sticky_note/sticky-colors';
-	import StickyNote from '$lib/components/sticky_note/StickyNote.svelte';
 	import ResizeableFrame from '$lib/components/window/frames/ResizeableFrame.svelte';
 	import StandardFrame from '$lib/components/window/frames/StandardFrame.svelte';
 	import Window from '$lib/components/window/Window.svelte';
-	import type { Position } from '$lib/utils/windows';
 
-	const heart = ({ x, y }: Position): boolean => {
-		// convert to [-1, 1]
-		x = x * 2 - 1;
-		y = y * 2 - 1;
+	// const heart = ({ x, y }: Position): boolean => {
+	// 	// convert to [-1, 1]
+	// 	x = x * 2 - 1;
+	// 	y = y * 2 - 1;
 
-		const C = 1 - Math.cbrt(x * x);
-		return C * Math.pow(19 - 8 * C, 2) - Math.pow(6 - 10 * C - 16 * y, 2) >= 1;
-	};
+	// 	const C = 1 - Math.cbrt(x * x);
+	// 	return C * Math.pow(19 - 8 * C, 2) - Math.pow(6 - 10 * C - 16 * y, 2) >= 1;
+	// };
 
-	const randPos = (): Position => {
-		let pos: Position;
-		do {
-			pos = {
-				x: Math.random(),
-				y: Math.random()
-			};
-		} while (!heart(pos));
-		return pos;
-	};
+	// const randPos = (): Position => {
+	// 	let pos: Position;
+	// 	do {
+	// 		pos = {
+	// 			x: Math.random(),
+	// 			y: Math.random()
+	// 		};
+	// 	} while (!heart(pos));
+	// 	return pos;
+	// };
 
-	const toCanvas = (pos: Position): Position => ({
-		x: 785 * pos.x,
-		y: 785 * (1 - pos.y)
-	});
+	// const toCanvas = (pos: Position): Position => ({
+	// 	x: 785 * pos.x,
+	// 	y: 785 * (1 - pos.y)
+	// });
 
-	const K = 100;
+	// const K = 100;
 </script>
 
 <MenuBar />
@@ -80,15 +77,24 @@
 	</svelte:fragment>
 </Window>
 
-{#each Array.from({ length: K }).keys() as i}
-	{#each Object.keys(stickyNoteColors) as color}
-		<StickyNote
-			text={`;)${i}`.slice(0, -1)}
-			{color}
-			startAt={toCanvas(randPos())}
-			size={{ w: 20, h: 15 }}
+<Window name="Schedule">
+	<slot slot="icon">
+		<img
+			class="w-[13px]"
+			draggable="false"
+			on:contextmenu={(e) => e.preventDefault()}
+			src="/assets/folders/default.png"
+			alt="Schedule Icon"
 		/>
-	{/each}
-{/each}
+	</slot>
+
+	<svelte:fragment let:size let:active>
+		<ResizeableFrame {size} {active} infinitelyWide={true}>
+			<div class="h-full w-full items-center justify-center">
+				<div class="h-24 w-24 bg-red-500"></div>
+			</div>
+		</ResizeableFrame>
+	</svelte:fragment>
+</Window>
 
 <ControlStrip />
