6294e2a7ae9de79b7ab9f1bd60439c67952f32a4
Alexander Wang <98280966+AlexanderWangY@users.noreply.github.com>
2024-11-09 23:12:27 -0500

    Title Screen
    * working on main title page
    
    * responsive to mobile
    
    * Customize

---
+++ src/lib/components/pages/Welcome.svelte
@@ -0,0 +1,52 @@
+<script>
+	import StandardFrame from '../window/frames/StandardFrame.svelte';
+
+	import Window from '../window/Window.svelte';
+</script>
+
+<Window name="Welcome!">
+	<slot slot="icon">
+		<img
+			class="w-[13px]"
+			draggable="false"
+			on:contextmenu={(e) => e.preventDefault()}
+			src="/assets/applications/website.png"
+			alt="Globe icon"
+		/>
+	</slot>
+
+	<svelte:fragment let:active>
+		<!-- Size is set to 0 to allow for text size to determine size of the window -->
+		<StandardFrame size={{ w: 0, h: 0 }} {active} infinitelyWide={false}>
+			<div
+				class="relative flex h-full w-full flex-col items-center justify-center gap-y-1 px-8 py-5 menu-md:px-10 menu-md:py-7 menu-lg:gap-y-3 menu-lg:px-12 menu-lg:py-14 menu-3xl:gap-y-6 menu-3xl:p-20"
+			>
+				<button
+					class="absolute bottom-0 left-0 h-1 w-1"
+					on:click={() => window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}
+				/>
+
+				<h1 class="title-text text-5xl menu-md:text-6xl menu-lg:text-8xl menu-3xl:text-9xl">
+					Swamphacks X
+				</h1>
+
+				<h2 class="title-text text-2xl menu-md:text-3xl menu-lg:text-5xl menu-3xl:text-6xl">
+					The University of Florida
+				</h2>
+
+				<h2 class="title-text text-xl menu-md:text-2xl menu-lg:text-4xl menu-3xl:text-5xl">
+					January 24-26th
+				</h2>
+
+				<a
+					class="link-text text-xl text-blue-600 underline menu-md:text-2xl menu-lg:text-4xl menu-3xl:text-5xl"
+					href="https://app.swamphacks.com/api/auth/signin"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Apply now!
+				</a>
+			</div>
+		</StandardFrame>
+	</svelte:fragment>
+</Window>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,94 +1,11 @@
 <script lang="ts">
 	import MenuBar from '$lib/components/menu_bar/MenuBar.svelte';
 	import ControlStrip from '$lib/components/control_strip/ControlStrip.svelte';
-	import { stickyNoteColors } from '$lib/components/sticky_note/sticky-colors';
-	import StickyNote from '$lib/components/sticky_note/StickyNote.svelte';
-	import ResizeableFrame from '$lib/components/window/frames/ResizeableFrame.svelte';
-	import StandardFrame from '$lib/components/window/frames/StandardFrame.svelte';
-	import Window from '$lib/components/window/Window.svelte';
-	import type { Position } from '$lib/utils/windows';
-
-	const heart = ({ x, y }: Position): boolean => {
-		// convert to [-1, 1]
-		x = x * 2 - 1;
-		y = y * 2 - 1;
-
-		const C = 1 - Math.cbrt(x * x);
-		return C * Math.pow(19 - 8 * C, 2) - Math.pow(6 - 10 * C - 16 * y, 2) >= 1;
-	};
-
-	const randPos = (): Position => {
-		let pos: Position;
-		do {
-			pos = {
-				x: Math.random(),
-				y: Math.random()
-			};
-		} while (!heart(pos));
-		return pos;
-	};
-
-	const toCanvas = (pos: Position): Position => ({
-		x: 785 * pos.x,
-		y: 785 * (1 - pos.y)
-	});
-
-	const K = 100;
+	import Welcome from '$lib/components/pages/Welcome.svelte';
 </script>
 
 <MenuBar />
 
-<Window name="Creative Assistant">
-	<slot slot="icon">
-		<img
-			class="w-[13px]"
-			draggable="false"
-			on:contextmenu={(e) => e.preventDefault()}
-			src="/assets/folders/colorsync_profiles.png"
-			alt="Assistant"
-		/>
-	</slot>
-
-	<svelte:fragment let:size let:active>
-		<StandardFrame {size} {active} infinitelyWide={true}>
-			testing<br />
-			testing<br />
-			testing<br />
-			really really really really really long
-		</StandardFrame>
-	</svelte:fragment>
-</Window>
-
-<Window name="Gymnast">
-	<slot slot="icon">
-		<img
-			class="w-[13px]"
-			draggable="false"
-			on:contextmenu={(e) => e.preventDefault()}
-			src="/assets/folders/system.png"
-			alt="Hullo"
-		/>
-	</slot>
-
-	<svelte:fragment let:size let:active>
-		<ResizeableFrame {size} {active} infinitelyWide={true}>
-			testing<br />
-			testing<br />
-			testing<br />
-			really really really really really long
-		</ResizeableFrame>
-	</svelte:fragment>
-</Window>
-
-{#each Array.from({ length: K }).keys() as i}
-	{#each Object.keys(stickyNoteColors) as color}
-		<StickyNote
-			text={`;)${i}`.slice(0, -1)}
-			{color}
-			startAt={toCanvas(randPos())}
-			size={{ w: 20, h: 15 }}
-		/>
-	{/each}
-{/each}
+<Welcome />
 
 <ControlStrip />
