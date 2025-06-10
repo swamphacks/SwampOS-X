0a494f9237240a3c0a0e3fb8dc5bdff7848565c7
h1divp <71522316+h1divp@users.noreply.github.com>
2025-06-10 01:19:55 -0400

    Added code for photo album link

--- src/lib/components/recap/Marquee.svelte
+++ src/lib/components/recap/Marquee.svelte
@@ -58,19 +58,17 @@
 			{/each}
 		</div>
 	</div>
-	<div class="items-center justify-center pt-6">
-	</div>
+	<div class="items-center justify-center pt-6"></div>
 
-<!-- Photo album link -->
+	<!-- Photo album link -->
 </section>
-	<section class="flex w-full items-center justify-center">
-		<a
-			class="link-text text-lg text-blue-600 underline underline-offset-4 menu-md:text-lg menu-lg:text-2xl menu-3xl:text-3xl"
-			href="https://photos.app.goo.gl/GiS4hwhuqVWmEJ7n7"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Photo Album
-		</a>
-	</section>
-
+<section class="flex w-full items-center justify-center">
+	<a
+		class="link-text text-lg text-blue-600 underline underline-offset-4 menu-md:text-lg menu-lg:text-2xl menu-3xl:text-3xl"
+		href="https://photos.app.goo.gl/GiS4hwhuqVWmEJ7n7"
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		Photo Album
+	</a>
+</section>
