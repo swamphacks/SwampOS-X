1084165b1674a46860ee1f028246d95aee6292a9
h1divp <71522316+h1divp@users.noreply.github.com>
2025-06-10 01:22:18 -0400

    edited wording

--- src/lib/components/recap/Stats.svelte
+++ src/lib/components/recap/Stats.svelte
@@ -5,7 +5,7 @@
 <section
 	class="title-text mx-auto grid w-full max-w-7xl grid-cols-1 gap-12 py-16 menu-md:py-24 menu-lg:grid-cols-3"
 >
-	<StatsNumber number={712} text="Applications" />
+	<StatsNumber number={712} text="Applicants" />
 	<StatsNumber number={273} text="Hackers" />
 	<StatsNumber number={75} text="Projects" />
 </section>
